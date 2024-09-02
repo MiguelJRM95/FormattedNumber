b4fc2b7bbd0bcfcb9839dfa145a83b5d5d464bbc
migueljrm95 <e.ogex.be2.dev.cdo@telefonica.com>
2024-09-02 13:32:04 +0200

    chore: code stiling scouting

--- src/parse-spanish-formatted-number.ts
+++ src/parse-spanish-formatted-number.ts
@@ -4,21 +4,16 @@ function parseSpanishFormattedNumber(input: string): string {
     if (!isSpanishFormattedNumber(input)) throw Error("Error");
     // Splits the number in two sides
     let [integer, decimal] = input.split(",");
-
     // Ensure the integer side can be replaced by just one 0
     if (Number.parseInt(integer) < 1){
         integer = "0";
     }
-
-
     // Checks if the decimal part is all zeros and removes it if so
     if (decimal && Number.parseInt(decimal) === 0) {
         decimal = "";
     }
-
     // Replace any dots in the integer side
     integer = integer.split('.').join('_');
-
     return decimal ? `${integer}.${decimal}` : integer;
 }
 
--- src/validate-spanish-formatted-number.ts
+++ src/validate-spanish-formatted-number.ts
@@ -1,26 +1,20 @@
 
 function isSpanishFormattedNumber(input: string): boolean {
     let decimalFound = false;
-    let indexOfDecimal = -1;
     let lastDotIndex = -1;
-
     const numberLength = input.length;
 
     for (let i = 0; i < numberLength; i++) {
         let char = input[i];
-
         if (char === ",") {
             // Comma can only be found once and must be after all dots
             if (decimalFound || lastDotIndex > -1 && i < lastDotIndex) return false;
             decimalFound = true;
-            indexOfDecimal = i;
         }
-
         if (char === ".") {
             // Ensure the dot is a valid thousand separator
             if (i - lastDotIndex !== 4 && lastDotIndex !== -1) return false;
             lastDotIndex = i;
-
             // Checks no comma appears before any dot
             if (decimalFound) return false;
         }
@@ -28,8 +22,6 @@ function isSpanishFormattedNumber(input: string): boolean {
 
     // After the loop, check if there are digits before the first dot and after the last dot
     return !(lastDotIndex !== -1 && (lastDotIndex < 1 || lastDotIndex > numberLength - 4));
-
-
 }
 
 export default isSpanishFormattedNumber;
