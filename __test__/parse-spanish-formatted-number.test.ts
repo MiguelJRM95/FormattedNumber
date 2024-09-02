ed0854030c51145b06b24159dce6080bde62ecdf
migueljrm95 <e.ogex.be2.dev.cdo@telefonica.com>
2024-09-02 13:30:18 +0200

    feat: format a number that is in spanish format

--- __test__/parse-spanish-formatted-number.test.ts
+++ __test__/parse-spanish-formatted-number.test.ts
@@ -2,14 +2,14 @@ import parseSpanishFormattedNumber from "../src/parse-spanish-formatted-number";
 
 describe('parseSpanishFormattedNumber', () => {
     test('SHOULD return the correct formatted number WHEN the number is a valid spanish number', () => {
-        expect(parseSpanishFormattedNumber("10,0")).toBe(10)
-        expect(parseSpanishFormattedNumber("10,000")).toBe(10)
-        expect(parseSpanishFormattedNumber("10.000")).toBe(10_000)
-        expect(parseSpanishFormattedNumber("10.000,01")).toBe(0.01)
-        expect(parseSpanishFormattedNumber("0.000,01")).toBe(0.01)
+        expect(parseSpanishFormattedNumber("10,0")).toBe("10")
+        expect(parseSpanishFormattedNumber("10,000")).toBe("10")
+        expect(parseSpanishFormattedNumber("10.000")).toBe("10_000")
+        expect(parseSpanishFormattedNumber("10.000,01")).toBe("10_000.01")
+        expect(parseSpanishFormattedNumber("0.000,01")).toBe("0.01")
     })
     test('SHOULD return an error WHEN the number is not a valid spanish number', () => {
-        expect(parseSpanishFormattedNumber("10.0")).toThrow("Error")
-        expect(parseSpanishFormattedNumber("10.00")).toThrow("Error")
+        expect(() => parseSpanishFormattedNumber("10.0")).toThrow("Error")
+        expect(() => parseSpanishFormattedNumber("10.00")).toThrow("Error")
     })
 })
--- src/parse-spanish-formatted-number.ts
+++ src/parse-spanish-formatted-number.ts
@@ -1,6 +1,25 @@
+import isSpanishFormattedNumber from "./validate-spanish-formatted-number";
 
 function parseSpanishFormattedNumber(input: string): string {
-    return ""
+    if (!isSpanishFormattedNumber(input)) throw Error("Error");
+    // Splits the number in two sides
+    let [integer, decimal] = input.split(",");
+
+    // Ensure the integer side can be replaced by just one 0
+    if (Number.parseInt(integer) < 1){
+        integer = "0";
+    }
+
+
+    // Checks if the decimal part is all zeros and removes it if so
+    if (decimal && Number.parseInt(decimal) === 0) {
+        decimal = "";
+    }
+
+    // Replace any dots in the integer side
+    integer = integer.split('.').join('_');
+
+    return decimal ? `${integer}.${decimal}` : integer;
 }
 
 export default parseSpanishFormattedNumber;
